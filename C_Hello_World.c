14e1601d9560823940da79e79aa940d718a3dfec
William Botura <williamdigitalwork@gmail.com>
2019-07-04 01:52:32 -0300

    First Commit and C Language Hello World

---
+++ C_Hello_World.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+//First method created using c.
+//In the edX course, they start off creating a int method with no parameter (void), and return a 0 int to the main method. This is rather stupid to do at first
+//so i'm going to do this my way
+
+//edX method
+//int main(void) 
+//{
+//	printf("Hello Wold");
+//	return 0;
+//}
+
+//my method
+void main() {
+	printf("Hello World!");
+}
